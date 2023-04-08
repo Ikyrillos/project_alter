f698f5d835e5ababa7cf921a94bc61ee104697cd
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-04-08 19:48:59 +0200

    added testing websocket

---
+++ routes/ws.dart
@@ -0,0 +1,25 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_web_socket/dart_frog_web_socket.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  final handler = webSocketHandler(
+    (channel, protocol) {
+      // A new client has connected to our server.
+      print('connected');
+
+      // Send a message to the client.
+      channel.sink.add('hello from the server');
+
+      // Listen for messages from the client.
+      channel.stream.listen(
+        (event) {
+          print('received: $event');
+        },
+        // The client has disconnected.
+        onDone: () => print('disconnected'),
+      );
+    },
+  );
+
+  return handler(context);
+}
