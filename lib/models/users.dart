6741a7a622e92817d6bcabac0f323f1dd645ce21
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-04-08 19:21:09 +0200

    clean code

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,5 +1,4 @@
-/// User class that represents a user object
-/// that is stored in the database
+// ignore_for_file: public_member_api_docs
 class User {
   /// User constructor that takes in a username and password
   User({
@@ -8,6 +7,7 @@ class User {
     required this.email,
     required this.id,
   });
+
   int id;
   String username;
   String password;
@@ -15,7 +15,7 @@ class User {
 
   /// save method that saves the user object to the database
   String toJson() {
-    return '{id: $id ,"username": "$username", "email": "$email"}';
+    return '{id: $id ,username: $username, email: $email}';
   }
 
   /// save method that saves the user object to the database
